1d217a5a60d6d91803946dc2f6a4989292ff0ce7
Tejo Nutalapati <tnutala@gmail.com>
2017-10-21 19:37:30 -0500

    comparison of exoplanet vs non exoplanet star

--- Tejo/exoplanetData/ExoplanetPlot.py
+++ Tejo/exoplanetData/ExoplanetPlot.py
@@ -10,11 +10,12 @@ from pandas import read_csv
 import matplotlib.pyplot as plt
 import numpy as np
 filename = 'exoTrain.csv'
-#dataframefull = read_csv(filename)
-dataframeZero= read_csv(filename, nrows=1) #gets just the data for star at index 0 
-array = dataframeZero.values
-X=array[0,1:]
-print(X)
+dataframefull = read_csv(filename)
+array= dataframefull.values
+#dataframeZero= read_csv(filename, nrows=1) #gets just the data for star at index 0 
+#array = dataframeZero.values
+X=array[4,1:]  #change first number to change star 
+Z=array[1000,1:] #change first number to change star 
 Y=np.arange(0,X.size,1)
 Y=[]
 for i in range(1,X.size+1):
@@ -22,9 +23,10 @@ for i in range(1,X.size+1):
 y=np.asarray(Y)
 X=np.reshape(X,(1,3197))
 Y=np.reshape(y,(1,3197))
-
+Z=np.reshape(Z,(1,3197))
 plt.plot(Y,X, 'ro')
+plt.plot(Y,Z, 'bo')
 plt.xlabel("time (arbitrary units)")
 plt.ylabel("flux reading")
-plt.title("Flux readings over time for Star 0")
+plt.title("Flux readings over time for Star X and Star Z")
 plt.show()
